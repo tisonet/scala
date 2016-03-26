3678d98e11e2641bc56acb10da5642a3b7872b5f
Zdenek Tison <you@example.com>
2016-03-26 20:39:17 +0100

    Added unit test for Memtable

---
+++ steladb/src/test/scala/tisonet/scala/steladb/MemtableSuite.scala
@@ -0,0 +1,37 @@
+package tisonet.scala.steladb
+
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+class MemtableSuite extends FunSuite with BeforeAndAfterEach {
+
+    val maxSize = 5
+    var table: Memtable = _
+
+    override def beforeEach() {
+        table = new ImmutableMemtable(maxSize)
+    }
+
+    test("Should return added value") {
+        table.add("key1", "some data")
+
+        assert(table.get("key1").get == "some data")
+    }
+
+    test("Should not be full when size does not exceed max size") {
+        table.add("key1", "some data")
+
+        assert(!table.isFull)
+    }
+
+    test("Should be full when size reach max size") {
+        (1 to maxSize).foreach(i => table.add("key" + i, "some data"))
+
+        assert(table.isFull)
+    }
+
+    test("Should be full when added the same key") {
+        (1 to maxSize) foreach (_ => table.add("key", "some data"))
+
+        assert(!table.isFull)
+    }
+}
