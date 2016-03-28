8a978e14fd0905b1f258afc5658466f5892f4e13
Zdenek Tison <Zdenek Tison>
2016-03-28 11:26:32 +0200

    SSTable - writes index and data to file

--- steladb/src/main/scala/tisonet/scala/steladb/sstable/LocalFileSSTable.scala
+++ steladb/src/main/scala/tisonet/scala/steladb/sstable/LocalFileSSTable.scala
@@ -8,54 +8,111 @@ import tisonet.scala.steladb.memtable.Memtable
 import scala.collection.mutable
 
 
-class LocalFileSSTable(override val memtable: Memtable, val filePath: String, val indexSize: Int)
-        extends SSTable {
+class LocalFileSSTable(override val memtable: Memtable, val filePath: String, val maxIndexSize: Int)
+    extends SSTable {
 
-    val metadataSize = getBytes(formatSize(0)).length
+    val NEW_LINE_DELIMITER = '\n'
+    val ENTRIES_DELIMITER = ':'
 
     override def flushToStorage(): String = {
 
         val index = mutable.Map[String, Long]()
-        val offset = 0L
-        var sstableFile: BufferedWriter = null
+        var sstableFile: RandomAccessFile = null
         val fileName = filePath + System.currentTimeMillis
+
+        var offset = 0L
+        var dataSize = 0L
+        var dataOffset = 0L
+        var indexOffset = 0L
+        var indexSize = 0L
 
         try {
             sstableFile = openFile(fileName)
 
-            for (entry <- sortedEntries) {
-                writeEntryToFileAndUpdateIndex(entry)
+            // Metadata section
+            shiftOffset(writeMetadata)
+
+            // Data section
+            shiftOffset(writeDataSection)
+            dataOffset = offset
+            for (entry <- sortedMemtableEntries) {
+                shiftOffset(writeDataEntryAndUpdateIndex(entry))
             }
-        }
-        finally
-            sstableFile close()
+            dataSize = offset - dataOffset
 
-        def writeEntryToFileAndUpdateIndex(entry: (String, String)): Long = {
+            // Index section
+            shiftOffset(writeIndexSection)
+            indexOffset = offset
+            for ((entry, position) <- sortedIndexEntries.zipWithIndex) {
+                if (shouldStoreEntryWithPosition(position)) {
+                    shiftOffset(writeIndexEntry(entry))
+                }
+            }
+            indexSize = offset - indexOffset
+
+            // Metadata update with correct offsets and sizes
+            seekToBeginningOfSstableFile()
+            writeMetadata
+        }
+        finally {
+            sstableFile.close()
+        }
+
+        def writeDataEntryAndUpdateIndex(entry: (String, String)): Long = {
             index.update(entry._1, offset)
-
-            val fileEntry = getFileDataForEntry(entry)
-            sstableFile.write(fileEntry)
-
-            offset + getBytes(fileEntry).length
+            writeToSSTable(getFileDataForEntry(entry), sstableFile)
         }
+
+        def writeIndexEntry(entry: (String, Long)): Long = {
+            writeLineToSSTable(entry._1 + ENTRIES_DELIMITER + entry._2, sstableFile)
+        }
+
+        def shiftOffset(shiftSize: Long) = offset += shiftSize
+
+        def writeDataSection: Long = writeLineToSSTable("==DATA==", sstableFile)
+
+        def writeIndexSection: Long = writeLineToSSTable("==INDEX==", sstableFile)
+
+        def writeMetadata: Long = {
+            writeLineToSSTable("==METADATA==", sstableFile) +
+                writeLineToSSTable("data:offset:%s:size:%s" format(formatSize(dataOffset), formatSize(dataSize)), sstableFile) +
+                writeLineToSSTable("index:offset:%s:size:%s" format(formatSize(indexOffset), formatSize(indexSize)), sstableFile)
+        }
+
+        def shouldStoreEntryWithPosition(indexEntryPosition: Int) =
+            (indexEntryPosition % Math.ceil(index.size / maxIndexSize.toDouble)) == 0
+
+        def sortedIndexEntries: Seq[(String, Long)] = {
+            index.toSeq.sortBy(_._1)
+        }
+        def seekToBeginningOfSstableFile(): Unit = sstableFile.seek(0)
 
         fileName
     }
 
-    private def sortedEntries = memtable.getAll.sortBy(_._1)
+    private def sortedMemtableEntries = memtable.getAll.sortBy(_._1)
+
+    private def writeLineToSSTable(data: String, sstableFile: RandomAccessFile): Long = {
+        writeToSSTable(data + NEW_LINE_DELIMITER, sstableFile)
+    }
+
+    private def writeToSSTable(data: String, sstableFile: RandomAccessFile): Long = {
+        val bytesToWrite = getBytes(data)
+        sstableFile.write(bytesToWrite)
+        bytesToWrite.length
+    }
 
     private def getFileDataForEntry(entry: (String, String)): String = {
-        val data = ":" + entry._1 + ":" + entry._2 + "\n"
+        val data = ENTRIES_DELIMITER + entry._1 + ENTRIES_DELIMITER + entry._2 + NEW_LINE_DELIMITER
         val dataSize = getBytes(data).length
-
         formatSize(dataSize) + data
     }
 
-    private def formatSize(size: Int) = "%06d".format(size)
+    private def formatSize(size: Long) = "%06d".format(size)
 
     private def getBytes(data: String) = data.getBytes(UTF_8)
 
-    private def openFile(fileName: String) = new BufferedWriter(new OutputStreamWriter(
-        new FileOutputStream(new File(fileName)), UTF_8))
+    private def openFile(fileName: String) = new RandomAccessFile(fileName, "rw")
+
 
 }
--- steladb/src/test/scala/tisonet/scala/steladb/sstable/SSTableSuite.scala
+++ steladb/src/test/scala/tisonet/scala/steladb/sstable/SSTableSuite.scala
@@ -11,7 +11,7 @@ class SSTableSuite extends FunSuite with BeforeAndAfterEach {
     var sstable: SSTable = _
     var memtable: Memtable = _
     var fileName: String = _
-    val filePath = "/tmp/test_sstable"
+    val filePath = "test_sstable"
 
     override def beforeEach() {
         memtable = new ImmutableMemtable()
@@ -26,10 +26,16 @@ class SSTableSuite extends FunSuite with BeforeAndAfterEach {
     test("Should flush data to file storage"){
         memtable.add("key1", "some data")
         memtable.add("key2", "other data")
+        memtable.add("key3", "other data")
+
+        val expected = "" +
+            "==METADATA==\ndata:offset:000085:size:000068\nindex:offset:000163:size:000017\n" +
+            "==DATA==\n000016:key1:some data\n000017:key2:other data\n000017:key3:other data\n" +
+            "==INDEX==\nkey1:85\nkey3:130\n"
 
         fileName = sstable.flushToStorage()
 
-        assert(readFile(fileName) == "000016:key1:some data\n000017:key2:other data\n")
+        assert(readFile(fileName) == expected)
     }
 
     def readFile (path: String): String = Source.fromFile(path, "UTF-8").mkString
