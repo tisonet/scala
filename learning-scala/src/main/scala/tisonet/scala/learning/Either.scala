1c3571f4af7210165ceec147595dc635ef4ccc46
Zdenek Tison <zdenek.tison@gmail.com>
2016-05-03 22:03:50 +0200

    Lazy Stream created

--- learning-scala/src/main/scala/tisonet/scala/learning/Either.scala
+++ learning-scala/src/main/scala/tisonet/scala/learning/Either.scala
@@ -3,17 +3,17 @@ package tisonet.scala.learning
 sealed trait Either [+E, +A] {
     def map[B] (f: A => B): Either[E, B] = this match {
         case Right(a) => Right(f(a))
-        case _ => _
+        case l@Left(e) => l
     }
 
     def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] = this match {
         case Right(a) => f(a)
-        case _ => _
+        case l@Left(e) => l
     }
 
     def orElse[EE >: E, B >: A](b: Either[EE, B]): Either[EE, B] = this match {
         case Left(e) => b
-        case _ => _
+        case r@Right(a) => r
     }
 
 }
